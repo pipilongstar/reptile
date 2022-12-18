80ade53d8b7fa3f525a679b2d92ed59de2c704ee
pipilong <2534158602@qq.com>
2022-12-18 19:33:55 +0800

    update

--- src/main/java/com/pipilong/controller/ItemController.java
+++ src/main/java/com/pipilong/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.pipilong.controller;
 
 import com.pipilong.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/item")
+@Slf4j
 public class ItemController {
 
     @Autowired
